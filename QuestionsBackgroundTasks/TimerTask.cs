3eacd80ea43a501674a3255e32de18784a780d1d
kiewic <kiewic@gmail.com>
2013-09-22 14:21:32 -0700

    Commenting out toast audio tag snippet.

--- QuestionsBackgroundTasks/TimerTask.cs
+++ QuestionsBackgroundTasks/TimerTask.cs
@@ -44,20 +44,22 @@ namespace QuestionsBackgroundTasks
             XmlDocument toastXml = ToastNotificationManager.GetTemplateContent(ToastTemplateType.ToastImageAndText02);
 
             // You can use the methods from the XML document to specify all of the
-            // required parameters for the toast
+            // required parameters for the toast.
             XmlNodeList stringElements = toastXml.GetElementsByTagName("text");
             stringElements.Item(0).AppendChild(toastXml.CreateTextNode("Push notification message:"));
             stringElements.Item(1).AppendChild(toastXml.CreateTextNode(messageReceived));
 
-            // Audio tags are not included by default, so must be added to the XML document.
-            string audioSrc = "ms-winsoundevent:Notification.IM";
-            XmlElement audioElement = toastXml.CreateElement("audio");
-            audioElement.SetAttribute("src", audioSrc);
+            // If comment out the next lines, the toast still makes noise.
+            //// Audio tags are not included by default, so must be added to the XML document.
+            //string audioSrc = "ms-winsoundevent:Notification.IM";
+            //XmlElement audioElement = toastXml.CreateElement("audio");
+            //audioElement.SetAttribute("src", audioSrc);
 
-            IXmlNode toastNode = toastXml.SelectSingleNode("/toast");
-            toastNode.AppendChild(audioElement);
+            //IXmlNode toastNode = toastXml.SelectSingleNode("/toast");
+            //toastNode.AppendChild(audioElement);
 
             // Create a toast from the Xml, then create a ToastNotifier object to show the toast.
+            var sdfg = toastXml.GetXml();
             ToastNotification toast = new ToastNotification(toastXml);
             ToastNotificationManager.CreateToastNotifier().Show(toast);
         }
